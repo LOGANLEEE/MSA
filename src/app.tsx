d1a7731acf0723b4a963e1c01c5b584d9d006f8b
LOGAN LEE <dlfjgkssk1@naver.com>
2021-03-30 21:46:23 +0900

    -add themeProvider -apply spring

--- src/app.tsx
+++ src/app.tsx
@@ -1,39 +1,48 @@
-import React from 'react';
+import React, { FunctionComponent } from 'react';
+import { ThemeProvider } from 'styled-components';
 import { useRecoilValue } from 'recoil';
 
 import { Header } from './Header';
 import { Body } from './Body';
 import { Footer } from './Footer';
-import { isDarkTheme } from 'globalState';
+import { theme_info_state } from 'globalState';
+import { useSpring } from 'react-spring';
 
 import { Wrapper } from './Wrapper';
-function App() {
+import { StylesProvider } from '@material-ui/core/styles';
+
+const App = () => {
 	document.title = 'Get infomation of how to be teacher in korea!';
-	const isDark = useRecoilValue(isDarkTheme);
+	// const isDark = useRecoilValue(isDarkTheme);
+	const spring_info = useSpring({ opacity: 1, from: { opacity: 0 } });
 
 	return (
-		<Wrapper isDark={isDark}>
-			<div className='app-block1' onClick={() => window.location.reload()}>
-				Like <br />
-				To <br />
-				Be <br />
-				Tea- <br />
-				cher
-			</div>
+		<StylesProvider injectFirst>
+			<ThemeProvider theme={useRecoilValue(theme_info_state)}>
+				<Wrapper style={spring_info}>
+					<div className='app-block1' onClick={() => window.location.reload()}>
+						Like <br />
+						To <br />
+						Be <br />
+						Tea- <br />
+						cher
+					</div>
 
-			<div className='app-block2'>
-				<div className='header'>
-					<Header />
-				</div>
-				<div className='body'>
-					<Body />
-				</div>
-				<div className='footer'>
-					<Footer />
-				</div>
-			</div>
-		</Wrapper>
+					<div className='app-block2'>
+						<div className='header'>
+							<Header />
+						</div>
+						<div className='body'>
+							<Body />
+						</div>
+						<div className='footer'>
+							<Footer />
+						</div>
+					</div>
+				</Wrapper>
+			</ThemeProvider>
+		</StylesProvider>
 	);
-}
+};
 
 export default App;
