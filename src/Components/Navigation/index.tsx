ff8b6797cfefa8f727492193affa641d1deb6037
LOGAN LEE <dlfjgkssk1@naver.com>
2021-03-30 21:50:31 +0900

    -custom navigation buttons

--- src/Components/Navigation/index.tsx
+++ src/Components/Navigation/index.tsx
@@ -1,5 +1,5 @@
 import React, { FunctionComponent } from 'react';
-import { Wrapper } from './Wrapper';
+import { Wrapper, StyledButton } from './Wrapper';
 
 import { Button } from '@material-ui/core';
 
@@ -8,18 +8,10 @@ interface Props {}
 export const Navigation: FunctionComponent = ({}: Props) => {
 	return (
 		<Wrapper>
-			<Button className='item' variant='contained' color='primary'>
-				Academy
-			</Button>
-			<Button size='large' className='item' variant='contained' color='secondary'>
-				News
-			</Button>
-			<Button className='item' variant='contained' color='default'>
-				Community
-			</Button>
-			<Button className='item' variant='contained' color='primary' href='#contained-buttons'>
-				Q&A
-			</Button>
+			<StyledButton className='item'>Academy</StyledButton>
+			<StyledButton className='item'>News</StyledButton>
+			<StyledButton className='item'>Community</StyledButton>
+			<StyledButton className='item'>Q&A</StyledButton>
 		</Wrapper>
 	);
 };
