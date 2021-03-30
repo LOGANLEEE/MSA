82bc3467884a1455a414754855768f20422d5cbc
LOGAN LEE <dlfjgkssk1@naver.com>
2021-03-30 21:49:55 +0900

    apply material-ui to styled-components

---
+++ src/Components/Login/Wrapper.ts
@@ -0,0 +1,11 @@
+import styled from 'styled-components';
+import Button from '@material-ui/core/Button';
+
+export const StyledButton = styled(Button)`
+	cursor: pointer;
+	background-color: #1f2738;
+	width: 80%;
+	height: 100%;
+`;
+
+export const Wrapper = styled.div``;
