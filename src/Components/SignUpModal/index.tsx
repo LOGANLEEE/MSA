893f357bb757f706311019b043d790de2e67c102
LOGAN LEE <dlfjgkssk1@naver.com>
2021-04-01 02:38:22 +0900

    add password validation add axios remove useless state

--- src/Components/SignUpModal/index.tsx
+++ src/Components/SignUpModal/index.tsx
@@ -5,6 +5,7 @@ import TextField from '@material-ui/core/TextField';
 import { ModalBodyWrapper, StyledModal, Wrapper, StyledButton } from './Wrapper';
 import Autocomplete from '@material-ui/lab/Autocomplete';
 import { CountryType, countries } from 'country';
+import { axios } from 'utils/globalFunc';
 
 interface Props {
 	should_open: boolean;
@@ -17,12 +18,7 @@ export const SignUpModal: FunctionComponent<Props> = ({ should_open, set_should_
 		to: { opacity: should_open ? 1 : 0 },
 	});
 
-	const [first_name, set_first_name] = useState('');
-	const [last_name, set_last_name] = useState('');
-	const [id, set_id] = useState('');
-	const [password, set_password] = useState('');
-	const [email, set_email] = useState('');
-	const [country_code, set_country_code] = useState('');
+	const [password_err, set_password_err] = useState(false);
 
 	// ISO 3166-1 alpha-2
 	// ⚠️ No support for IE 11
@@ -31,14 +27,18 @@ export const SignUpModal: FunctionComponent<Props> = ({ should_open, set_should_
 			? isoCode.toUpperCase().replace(/./g, (char) => String.fromCodePoint(char.charCodeAt(0) + 127397))
 			: isoCode;
 	}, []);
+
 	// FormEventHandler<HTMLButtonElement>
 	const validation = useCallback((e) => {
 		e.preventDefault();
-		const formData: any = new FormData(e.target);
-
-		for (var [key, value] of formData.entries()) {
-			console.log(key, value);
+		const formData: FormData = new FormData(e.target);
+		if ((formData.get('password') as string).length < 5) {
+			set_password_err(true);
+			return;
 		}
+		set_password_err(false);
+
+		axios.post('/auth/signup', Object.fromEntries(formData));
 	}, []);
 
 	return (
@@ -53,44 +53,37 @@ export const SignUpModal: FunctionComponent<Props> = ({ should_open, set_should_
 					<form onSubmit={validation}>
 						<div className='item1'>
 							<TextField
-								onChange={({ target: { value } }) => set_first_name(value)}
+								// onChange={({ target: { value } }) => set_first_name(value)}
 								label='First Name'
-								name='First Name'
+								name='first_name'
 								type='text'
-								id='First Name'
-								error={first_name === ''}
 								autoFocus
 								required
 							/>
 							<TextField
-								onChange={({ target: { value } }) => set_last_name(value)}
+								// onChange={({ target: { value } }) => set_last_name(value)}
 								label='Last Name'
-								name='Last Name'
-								id='Last Name'
+								name='last_name'
 								type='text'
-								error={last_name === ''}
 								required
 							/>
 						</div>
 						<br />
 						<TextField
 							label='ID'
-							name='ID'
-							id='ID'
+							name='id'
 							type='text'
 							required
-							error={id === ''}
-							onChange={({ target: { value } }) => set_id(value)}
+							// onChange={({ target: { value } }) => set_id(value)}
 						/>
 						<br />
 						<TextField
 							label='Password (6 character or more)'
 							type='password'
-							id='password'
 							name='password'
+							error={password_err}
 							autoComplete='current-password'
-							error={password === ''}
-							onChange={({ target: { value } }) => set_password(value)}
+							// onChange={({ target: { value } }) => set_password(value)}
 							required
 						/>
 						<br />
@@ -99,17 +92,15 @@ export const SignUpModal: FunctionComponent<Props> = ({ should_open, set_should_
 							label='E-Mail'
 							required
 							type='email'
-							id='E-Mail'
-							name='E-Mail'
-							error={email === ''}
-							onChange={({ target: { value } }) => set_email(value)}
+							name='email'
+							// onChange={({ target: { value } }) => set_email(value)}
 						/>
 						<br />
 						<br />
 						<Autocomplete
 							options={countries as CountryType[]}
 							autoHighlight
-							onChange={(e: any, { code }: any) => set_country_code(code)}
+							// onChange={(e: any, { code }: any) => set_country_code(code)}
 							getOptionLabel={(option) => option.label}
 							renderOption={(option) => (
 								<>
@@ -121,10 +112,8 @@ export const SignUpModal: FunctionComponent<Props> = ({ should_open, set_should_
 								<TextField
 									{...params}
 									label='Choose a country'
-									id='coutnry'
 									name='coutnry'
 									required
-									error={country_code === ''}
 									// error={country_error}
 									inputProps={{
 										...params.inputProps,
